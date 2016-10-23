e69c6b91ea374049552bedbf9211fddea44c65c1
rafeu <duchainer@gmail.com>
2016-10-23 18:26:18 -0400

    Changed Cours to Evaluations in Eleve Ajout d'un constructeur pour avoir des notes "aléatoires" pour l'élève

--- GestiNotes2.0/src/Eleve.java
+++ GestiNotes2.0/src/Eleve.java
@@ -3,35 +3,49 @@ import java.util.*;
 
 /**
  * 28/09/2016
+ *
  * @author raphael
  */
 class Eleve {
 //variables
-    private String nom, prenom,dateNaissance;
-    private ArrayList<Evaluation> cours= new ArrayList<Evaluation>();
-    
+
+    private String nom, prenom, dateNaissance;
+    private ArrayList<Evaluation> Evaluations = new ArrayList<Evaluation>();
+
 //Constructeur
-    Eleve(){
-       this("Etychen","Paul","01-04-1999");
+    Eleve() {
+        this("Etychen", "Paul", "01-04-1999");
     }
-    
-    Eleve(String nom,String prenom,String dateNaissance){
+
+    Eleve(String nom, String prenom, String dateNaissance) {
         setNom(nom);
         setPrenom(prenom);
         setDateNaissance(dateNaissance);
     }
 
-    Eleve(String nom, String prenom, String date, Evaluation[] listeCours) {
-        this(nom,prenom,date);
-        for (int i =0; i<listeCours.length;i++){
-            cours.add(listeCours[i]);
+    Eleve(String nom, String prenom, String date, Evaluation[] listeEvaluation) {
+        this(nom, prenom, date);
+        for (int i = 0; i < listeEvaluation.length; i++) {
+            Evaluations.add(listeEvaluation[i]);
         }
     }
-    
+
+    Eleve(String nom, String prenom, String date, Evaluation[] listeEvaluation, boolean random) {
+        this(nom,prenom,date,listeEvaluation);
+        //Si l'on veut générer une note "au hasard"
+        if (random) {
+            for (int i = 0; i < listeEvaluation.length; i++) {
+                Evaluations.get(i).setNote((double)100-getNom().length()*2);
+            }
+        }
+
+    }
+
 //Accesseur-Mutateurs (GET-SET)
     public String getNom() {
         return nom;
     }
+
     public void setNom(String nom) {
         this.nom = nom;
     }
@@ -39,6 +53,7 @@ class Eleve {
     public String getPrenom() {
         return prenom;
     }
+
     public void setPrenom(String prenom) {
         this.prenom = prenom;
     }
@@ -46,41 +61,45 @@ class Eleve {
     public String getDateNaissance() {
         return dateNaissance;
     }
+
     public void setDateNaissance(String dateNaissance) {
         this.dateNaissance = dateNaissance;
     }
 
     public int getNbrCours() {
-        return Etablissement.listeCours.length;
+        return Etablissement.listeEvaluations.length;
     }
 
     public ArrayList<Evaluation> getTabCours() {
-        return cours;
+        return Evaluations;
     }
+
     public void setTabCours(ArrayList<Evaluation> cours) {
-        this.cours = cours;
+        this.Evaluations = cours;
     }
+
     public void addCours(Evaluation cours) {
-        this.cours.add(cours);
+        this.Evaluations.add(cours);
     }
-    
+
 //Autres méthodes
-    public String codePermanent(){
+    public String codePermanent() {
         //DUchaine Raphael 29-04-1998 DR1998
-        return getNom().substring(0,1)+getPrenom().substring(0, 1)+getDateNaissance().substring(6, 10);
+        return getNom().substring(0, 1) + getPrenom().substring(0, 1) + getDateNaissance().substring(6, 10);
     }
 
     double calculerNoteFinale() {
-        double somme =0;
-        for (int i=0;i<cours.size();i++){
-            Evaluation my_cours = cours.get(i);
-            somme +=(my_cours.getNote()*my_cours.getValeurCours());
+        double somme = 0;
+        for (int i = 0; i < Evaluations.size(); i++) {
+            Evaluation my_cours = Evaluations.get(i);
+            somme += (my_cours.getNote() * my_cours.getValeurCours());
         }
         return somme;
     }
+
     @Override
-    public String toString(){ 
-       return (nom+", "+prenom+", "+dateNaissance+", "+calculerNoteFinale()+"% "+codePermanent());
+    public String toString() {
+        return (nom + ", " + prenom + ", " + dateNaissance + ", " + calculerNoteFinale() + "% " + codePermanent());
     }
-    
+
 }
