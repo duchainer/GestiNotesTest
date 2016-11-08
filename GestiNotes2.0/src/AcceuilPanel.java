fc24df849f44e1a87d3db1c41e7b6a9ed9b90162
rafeu <duchainer@gmail.com>
2016-11-07 19:27:59 -0500

    Ajout de AcceuilPanel et de feels.png

---
+++ GestiNotes2.0/src/AcceuilPanel.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Crée 2016-11-07,19:22:32 
+ * @author Raphael Duchaine
+ */
+public class AcceuilPanel extends UtilePanel{
+         //variables
+    
+    
+         //Méthodes
+
+    //Constructeur
+
+    //Get-Set
+
+    //toString
+
+    //Autres Méthodes
+
+    
+}
