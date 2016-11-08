89e222e6f6e1f30e262a2971efe34ae5e273eb6e
rafeu <duchainer@gmail.com>
2016-11-07 19:39:35 -0500

    Ajout de l'onglet "Acceuil" contenant un new AcceuilPanel

--- GestiNotes2.0/src/PrincipaleFrame.java
+++ GestiNotes2.0/src/PrincipaleFrame.java
@@ -45,6 +45,7 @@ public class PrincipaleFrame extends UtileFrame {
         simplePanel = new JPanel(); 					//Cree le panneau
         JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
         simplePanel.add(tabbedPane);
+        tabbedPane.addTab("Acceuil", new AcceuilPanel(this));
         tabbedPane.addTab("Menu Eleve", new ElevePanel(this));
         tabbedPane.addTab("Menu Aide", new AidePanel(this));
 
