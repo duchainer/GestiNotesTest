809b6275e06258e5bd390678682482faa29295d6
patrickd4868 <patrickd4868@gmail.com>
2016-11-09 16:24:32 -0500

    Ajout d'un message d'accueil

--- GestiNotes2.0/src/vue/AcceuilPanel.java
+++ GestiNotes2.0/src/vue/AcceuilPanel.java
@@ -4,6 +4,7 @@ package vue;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.*;
 
 /**
  * Crée 2016-11-07,19:22:32
@@ -25,6 +26,11 @@ public class AcceuilPanel extends UtilePanel {
         Graphics2D g2d = (Graphics2D) g;
         Image image = getToolkit().getImage("images/bille.png");    // chercher fichier image
         g2d.drawImage(image, 2*570/3-125, 750/4, 570/2, 750/2, this);        // le dessiner avec coordonnees et dimensions
+        g2d.setFont( new Font( "Algerian", Font.BOLD, 20 ) );
+        g2d.drawString("Bienvenue dans le", 15, 250);
+        g2d.drawString("gestionnaire GestiNotes", 15, 270);
+        g2d.drawString("version 2.0!", 15, 290);
+        
     }
     //Get-Set
 
