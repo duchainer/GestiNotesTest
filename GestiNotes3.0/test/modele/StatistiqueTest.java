d182598ff9e2e7dc1b7489b01115aaa128487c92
rafeu <duchainer@gmail.com>
2016-11-21 09:38:38 -0500

    Début Test de Statistique

---
+++ GestiNotes3.0/test/modele/StatistiqueTest.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modele;
+
+import static modele.Etablissement.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+
+/**
+ * Créé fièrement 2016-11-20, 16:12:56
+ * @author Raphael Duchaine
+ */
+public class StatistiqueTest {
+    Groupe groupe;
+    public StatistiqueTest() {
+         groupe= new Groupe(0);
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of calculerMoyenne method, of class Statistique.
+     */
+    @Test
+    public void testCalculerMoyenneSansNotes() {
+        System.out.println("calculerMoyenneSansNotes");
+        for (int i = 0; i < noms.length; i++) {
+            groupe.addEleve(new Eleve(noms[i], prenoms[i], dates[i]));
+//            groupe.getTabEleve().get(i).setNote(0, i);
+        }
+        float expResult = 0F;
+        float result = Statistique.calculerMoyenne(groupe);
+        assertEquals(expResult, result, 0.0);
+    }
+    @Ignore
+    public void testCalculerMoyenne2() {
+        System.out.println("calculerMoyenne2");
+        for (int i = 0; i < noms.length; i++) {
+            groupe.addEleve(new Eleve(noms[i], prenoms[i], dates[i]));
+            groupe.getTabEleve().get(i).setNote(0, i);
+        }
+        float expResult = 0F;
+        float result = Statistique.calculerMoyenne(groupe);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    /**
+     * Test of calculerEcartType method, of class Statistique.
+     */
+    @Test
+    public void testCalculerEcartTypeSansNotes() {
+        System.out.println("calculerEcartTypeSansNotes");
+        for (int i = 0; i < noms.length; i++) {
+            groupe.addEleve(new Eleve(noms[i], prenoms[i], dates[i]));
+//            groupe.getTabEleve().get(i).setNote(0, i);
+        }
+        float expResult = 0F;
+        float result = Statistique.calculerEcartType(groupe);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    /**
+     * Test of calculerVariance method, of class Statistique.
+     */
+    @Test
+    public void testCalculerVariance() {
+        System.out.println("calculerVarianceSansNotes");
+        for (int i = 0; i < noms.length; i++) {
+            groupe.addEleve(new Eleve(noms[i], prenoms[i], dates[i]));
+//            groupe.getTabEleve().get(i).setNote(0, i);
+        }
+        float expResult = 0F;
+        float result = Statistique.calculerVariance(groupe);
+        assertEquals(expResult, result, 0.0);
+    }
+
+}
