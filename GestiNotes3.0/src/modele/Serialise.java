2b8ff6f712c1a0820a11b5768fbc6ffadfbb94f5
rafeu <duchainer@gmail.com>
2016-11-28 14:26:49 -0500

    Creation des methodes initialise,importe et exporteGroupes

--- GestiNotes3.0/src/modele/Serialise.java
+++ GestiNotes3.0/src/modele/Serialise.java
@@ -9,18 +9,23 @@ import javax.swing.JOptionPane;
 
 public class Serialise {
 
-    public static void main(String[] args) throws IOException {
-        // Pour �crire dans le fichier objet:
-        Etablissement.initialise();
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        // Pour ecrire dans le fichier objet:
+        /*Etablissement.initialise();
         ArrayList<Groupe> groupes = Etablissement.getTabGroupe();
-        WriteTabGroupe(groupes);
+        exporteGroupes(groupes);*/
         
-        try {
-            Etablissement.setTabGroupe(ReadTabGroupe());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        initialiseGroupes();
+        
+        for (Groupe groupe : Etablissement.getTabGroupe()) {
+            //affichage de ses attributs
+            System.out.println("---------------");
+            System.out.println("Numero"+groupe.getNumero());
+            System.out.println("Eleves: "+groupe.listeEleve());
         }
-
+        System.out.println("-");
+        
+        importeGroupes();
 
         for (Groupe groupe : Etablissement.getTabGroupe()) {
             //affichage de ses attributs
@@ -30,68 +35,72 @@ public class Serialise {
         }
     }
     
-    public static void WriteTabGroupe(ArrayList<Groupe> groupes) throws IOException {
+    /**
+     *
+     * @param groupes
+     * @throws IOException
+     */
+    public static void exporteGroupes(ArrayList<Groupe> groupes) throws IOException {
         // Ouverture du flux objet en sortie
         ObjectOutputStream sortie = new ObjectOutputStream(
                 new FileOutputStream("groupes.txt"));
-        // �criture de l'objet dans le fichier
+        // ecriture de l'objet dans le fichier
             sortie.writeObject(groupes);
             sortie.flush();
         // fermer flux
         sortie.close();
     }
 
-    public static ArrayList<Groupe> ReadTabGroupe() throws IOException,ClassNotFoundException,FileNotFoundException {
+    /**
+     * Lit un objet serialisé dans un fichier 
+     * @param filename the value of filename
+     * @throws IOException
+     * @throws ClassNotFoundException
+     * @throws FileNotFoundException
+     */
+    public static ArrayList<Groupe> ReadTabGroupe(final String filename) throws IOException,ClassNotFoundException,FileNotFoundException {
         ObjectInputStream entree = null;
         ArrayList<Groupe> retour = new ArrayList<Groupe>();
         try {
             //Pour lire le fichier objet :
-            // Ouverture du flux objet en entr�e
+            // Ouverture du flux objet en entree
             entree = new ObjectInputStream(
-                    new FileInputStream("groupes.txt"));
+                    new FileInputStream(filename));
             // Lecture de l'objet contenu dans le fichier
                 retour=(ArrayList<Groupe>) entree.readObject();
         }catch (FileNotFoundException e) {
-            // Exception d�clench�e si le fichier n'existe pas
-            printStackTrace(e);
+            // Exception declenchee si le fichier n'existe pas
+            printError(e);
         } catch (EOFException e) {
-            // Exception d�clench�e si la fin du fichier est atteinte
+            // Exception declenchee si la fin du fichier est atteinte
             System.out.println("Fin du fichier");
             System.out.println("Aucun Objet");
         } catch (IOException e) {
-            // Exception d�clench�e si un autre probl�me acc�s fichier
-            printStackTrace(e);
+            // Exception declenchee si un autre probleme acces fichier
+            printError(e);
         } catch (ClassNotFoundException e) {
-            printStackTrace(e);
+            printError(e);
         } finally {
             try {
                 entree.close();
             } catch (IOException e) {
-                printStackTrace(e);
+                printError(e);
             } catch (Exception e) {
-                printStackTrace(e);
+                printError(e);
             }
         }
         return retour;
     }
 
-    /*public static ArrayList<Groupe> demanderGroupes() throws NumberFormatException, HeadlessException {
-        //Cr�ation d'objets de la classe vente (qui est s�rialisable)
-        ArrayList<Groupe> retour = new ArrayList<Groupe>();
-        int reponse=0;
-        do {
-            
-            String numero =JOptionPane.showInputDialog(null, "Entrer le numero de vente.");
-            int commission=Integer.parseInt(JOptionPane.showInputDialog(
-                    null, "Entrer la commission de la vente."));
-            retour.add(new Groupe(numero, commission));
-            reponse = JOptionPane.showConfirmDialog(null, "Voulez-vous entrer une autre vente?",
-                    "Continuer?", JOptionPane.YES_NO_OPTION);
-        } while (reponse != JOptionPane.NO_OPTION);
-        return retour;
-    }*/
+    public static void importeGroupes() throws IOException, ClassNotFoundException {
+            Etablissement.setTabGroupe(ReadTabGroupe("groupes.txt"));
+    }
+    
+    public static void initialiseGroupes() throws IOException, ClassNotFoundException {
+            Etablissement.setTabGroupe(ReadTabGroupe("initialisation.txt"));
+    }
 
-    private static void printStackTrace(Exception e) {
+    private static void printError(Exception e) {
         System.err.println(e.getCause());
     }
 }
